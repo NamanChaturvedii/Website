107f7d6811eaf05496f88a0bd2a22e4f43b9bcba
Naman 'Chaturvedi <namanchaturvedi58@gmail.com>
2023-06-18 20:39:29 +0530

    ecommerce web

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from "express"
+const app = express();
+import dotenv from "dotenv"
+import morgan from "morgan"
+dotenv.config()
+import authRoutes from "./routes/auth.js";
+import catagoryRoute from "./routes/categoryRoute.js"
+import productRoute from "./routes/productRoutes.js"
+import connectDB from "./config/db.js";
+import cors from "cors"
+connectDB();
+
+
+const PORT= process.env.PORT ||8000;
+
+// rest api
+
+// middle
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"))
+
+app.use("/api/v1/auth",authRoutes)
+app.use("/api/v1/category",catagoryRoute)
+app.use("/api/v1/product",productRoute)
+
+
+app.get("/",(req,res)=>{
+    res.send("you are at home page")
+})
+
+
+app.listen(PORT,()=>{
+    console.log("running ")
+})
+
+
